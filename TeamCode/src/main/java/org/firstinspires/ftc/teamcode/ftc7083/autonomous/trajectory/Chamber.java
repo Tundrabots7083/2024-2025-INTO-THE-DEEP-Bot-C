25893d6bfe541489870d90774e5c47f2f0b7eafe
Roy Brabson <rbrabson@gmail.com>
2025-02-15 18:49:57 -0500

    three specimen

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/autonomous/trajectory/Chamber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/autonomous/trajectory/Chamber.java
@@ -37,21 +37,21 @@ public class Chamber {
     public static double SPIKE_MARK_1_ORIENTATION = -55;
     public static double SPIKE_MARK_1_X = 18;
     public static double SPIKE_MARK_1_OBSERVATION_ZONE_X = 40;
-    public static double SPIKE_MARK_1_OBSERVATION_ZONE_Y = -41;
+    public static double SPIKE_MARK_1_OBSERVATION_ZONE_Y = -42;
     public static double SPIKE_MARK_1_OBSERVATION_ZONE_ORIENTATION = -65;
 
     // Pickup specimen 2 from wall
-    public static double SPECIMEN_2_INTAKE_AT_WALL_STAGE_1_Y = -43;
+    public static double SPECIMEN_2_INTAKE_AT_WALL_STAGE_1_Y = -44;
     public static double SPECIMEN_2_INTAKE_AT_WALL_STAGE_1_ORIENTATION = -65;
 
     // Position for scoring specimen 2 on the high chamber
     public static double SPECIMEN_2_CHAMBER_X = -2;
     public static double SPECIMEN_2_CHAMBER_Y = -50.5;
     public static double SPECIMEN_2_CHAMBER_ORIENTATION = SPECIMEN_1_CHAMBER_ORIENTATION;
-    public static double SPECIMEN_2_CHAMBER_SCORE_Y = -48.5;
+    public static double SPECIMEN_2_CHAMBER_SCORE_Y = -49;
 
     // Pickup specimen 3 from wall
-    public static double SPECIMEN_3_REVERSE_FROM_CHAMBER_Y = -48;
+    public static double SPECIMEN_3_REVERSE_FROM_CHAMBER_Y = -50;
     public static double SPECIMEN_3_INTAKE_AWAY_FROM_WALL_X = 37.5;
     public static double SPECIMEN_3_INTAKE_AWAY_FROM_WALL_Y = -38;
     public static double SPECIMEN_3_INTAKE_AWAY_FROM_WALL_ORIENTATION = -50;
@@ -61,14 +61,14 @@ public class Chamber {
     public static double SPECIMEN_3_INTAKE_AT_WALL_STAGE_2_ORIENTATION = -45;
 
     // Position for scoring specimen 3 on the high chamber
-    public static double SPECIMEN_3_CHAMBER_X = 0;
-    public static double SPECIMEN_3_CHAMBER_Y = -49;
+    public static double SPECIMEN_3_CHAMBER_X = 4;
+    public static double SPECIMEN_3_CHAMBER_Y = -53;
     public static double SPECIMEN_3_CHAMBER_ORIENTATION = 135;
-    public static double SPECIMEN_3_CHAMBER_SCORE_Y = -47;
+    public static double SPECIMEN_3_CHAMBER_SCORE_Y = -51;
 
     // Park in the observation zone
-    public static double PARK_X = 50;
-    public static double PARK_Y = -35;
+    public static double PARK_X = 40;
+    public static double PARK_Y = -40;
     public static double PARK_ORIENTATION = 140;
 
     private final TrajectoryActionBuilder actionBuilder;
@@ -146,17 +146,17 @@ public class Chamber {
                 .lineToYLinearHeading(SPECIMEN_3_INTAKE_AT_WALL_STAGE_1_Y, Math.toRadians(SPECIMEN_3_INTAKE_AT_WALL_STAGE_1_ORIENTATION))
 //                .waitSeconds(INTAKE_SPECIMEN_WALL_TIMEOUT)
 //                .lineToYLinearHeading(SPECIMEN_3_INTAKE_AT_WALL_STAGE_2_Y, Math.toRadians(SPECIMEN_3_INTAKE_AT_WALL_STAGE_2_ORIENTATION))
-//                .stopAndAdd(ias.actionIntakeSpecimenFromWall())
+                .stopAndAdd(ias.actionIntakeSpecimenFromWall())
 
-//                // Move to the chamber and score specimen 3
-//                .strafeToSplineHeading(new Vector2d(SPECIMEN_3_CHAMBER_X, SPECIMEN_3_CHAMBER_Y), Math.toRadians(SPECIMEN_3_CHAMBER_ORIENTATION))
-//                .stopAndAdd(ias.actionMoveToScoreSpecimenHighChamber())
-//                .lineToY(SPECIMEN_3_CHAMBER_SCORE_Y)
-//                .stopAndAdd(ias.actionScoreSpecimenHighChamber())
+                // Move to the chamber and score specimen 3
+                .strafeToSplineHeading(new Vector2d(SPECIMEN_3_CHAMBER_X, SPECIMEN_3_CHAMBER_Y), Math.toRadians(SPECIMEN_3_CHAMBER_ORIENTATION))
+                .stopAndAdd(ias.actionMoveToScoreSpecimenHighChamber())
+                .lineToY(SPECIMEN_3_CHAMBER_SCORE_Y)
+                .stopAndAdd(ias.actionScoreSpecimenHighChamber())
 
                 // Park the robot
-//                .strafeToSplineHeading(new Vector2d(PARK_X, PARK_Y), Math.toRadians(PARK_ORIENTATION))
-//                .stopAndAdd(ias.actionMoveToStartPosition())
+                .strafeToSplineHeading(new Vector2d(PARK_X, PARK_Y), Math.toRadians(PARK_ORIENTATION))
+                .stopAndAdd(ias.actionMoveToStartPosition())
                 .waitSeconds(15)
                 .build();
     }
